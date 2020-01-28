2389ecda5829b97632c45385a1c355fd45c9cbff
jmauro33 <jonathanmauro16@yahoo.com>
2020-01-28 11:59:33 -0600

    getting js code to run properly

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-var weather = $(this).attr("data-weather");
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=dddc28a98071661cf0d3e17f3a345892";
 
-var getWeather = function() {
+
+var weather = $(this).attr("data-weather");
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=kansas+city&APPID=dddc28a98071661cf0d3e17f3a345892";
+
+var getWeather = function () {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
+
+
+
     console.log(response)
-    
-    // get the parts of the data you need
-    // var temp = response.main.temp;
 
 
-    // var description = response.weather[0].description;
-
-
-    // var place = response.name;
-    
-
-    // select your dom elements and append the data
-    // var weatherDiv = $('<div>')
-    // var p = $('<p>').text(results[i].rating)
-    // var weatherImage = $('<img>').attr()
-    // weatherDiv.append(p)
-    // $().prepend(weatherDiv);
   });
 }
 
-getWeather()
+  // get the parts of the data you need
+  
+  // select your dom elements and append the data
+
+  function getWeather(data){
+
+     var temp = response.main.temp;
+     var desc = response.weather[0].description;
+     var place = response.name.place;
+
+      console.log(data)
+
+  };
+
+     var weather = $('<div>')
+     var p = $('<p>').text(weather.data)
+     var weatherImage = $('<img>').attr()
+     weatherDiv.append(p)
+     $("#weatherdata").prepend(weather);
+  
+  
+
+
+
+
+
+
+
+
