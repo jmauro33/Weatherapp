c9977584b33296a45d9531993c07457d7f733faf
jmauro33 <jonathanmauro16@yahoo.com>
2020-01-28 16:13:20 -0600

    still working on js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 var weather = $(this).attr("data-weather");
 var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=kansas+city&APPID=dddc28a98071661cf0d3e17f3a345892";
 
@@ -7,29 +6,50 @@ var getWeather = function () {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function (response) {
+
+  }).then(function getWeather (response) {
 
     console.log(response)
 
+  for (var i= 0; i < weather.response; i++)
+
+  var response = main.response.temp;
+  var response = data.weather[0].description
+  var response = data.place;
+
+    
   });
 }
 
-function getWeather(data) {
+function showCity (city) {
+
+  console.log(city)
+
+  var city = main.city;
+  
+  for (var i = 0; i < main.city; i++)
+
+  var city = location;
+}
+
+
+
+
+
+function showWeather(data) {
 
    console.log(data)
 
-    var temp = response.main.temp;
-    var desc = response.weather[0].description;
-    var place = response.name.place;
-    
+    var data = main.data.temp;
+    var data = data.weather[0].description;
+    var data = data.place;
+
 
    for (var i = 0; i < weather.data; i++) 
    
     var weather = $('<div>')
     var p = $('<p>').text(weather.data)
-    var weatherImage = $('<img>').attr()
     weather.append(p)
-    $("#weatherdata").prepend(weather)
-
+    $("#data-weather").prepend(weather)
 
 };
