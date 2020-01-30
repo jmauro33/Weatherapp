5b1c4e4f16b5bdc4b5fcd2a60160a27c5501f7af
jmauro33 <jonathanmauro16@yahoo.com>
2020-01-30 09:07:18 -0600

    got api and js to work

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-var APIKey = "ddc28a98071661cf0d3e17f3a345892";
+var APIKey = "2dfc798628fe4b23a5ba491ed0f74479";
 
 var weather = $(this).attr("data-weather");
 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-"q=Kansas+City,Missouri&units=imperial&appid=" + APIKey;
-
+"q=kansas+city&units=imperial&APPID=" + APIKey;
+"https://api.openweathermap.org/data/2.5/weather?q=kansas+city&units=imperial&APPID=2dfc798628fe4b23a5ba491ed0f74479"
 function getWeather (response){
 
   $.ajax({
@@ -36,8 +36,50 @@ function getWeather (response){
   console.log("Wind Speed: " + response.wind.speed);
   console.log("Humidity: " + response.main.humidity);
   console.log("Temperature (F): " + response.main.temp);
-});
+getForecast()
+
+})
+.catch(function(error){
+  console.log(error)
+})
 
 };
 
 
+getWeather()
+
+
+var forecastqueryURL = "https://api.openweathermap.org/data/2.5/forecast?" +
+"q=kansas+city&units=imperial&APPID=" + APIKey;
+
+function getForecast(response){
+
+  $.ajax({
+    url: forecastqueryURL,
+    method: "GET"
+
+  }).then(function(response) {
+
+
+    console.log(forecastqueryURL);
+  
+  
+    console.log(response);
+    var cardnum = 1
+  for (i = 0; i < response.list.length; i++){
+    if(response.list[i].dt_txt.indexOf("15:00:00")!==-1){
+      var p = $("<p>").text("temp: " + response.list[i].main.temp_max)
+      var p2 = $("<p>").text("weather: " + response.list[i].weather[0].description)
+      $(".card-"+cardnum).append(p)
+      $(".card-"+cardnum).append(p2)
+      cardnum++
+    }
+  }
+ 
+
+  })
+.catch(function(error){
+  console.log(error)
+});
+
+}
